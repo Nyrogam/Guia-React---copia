6af76065088b771e2ca8a0920f438f7eae1c6a18
Ignacio Pino <pino.v.pablo@gmail.com>
2021-07-29 18:21:54 -0300

    test

--- React/presupuestos/src/components/Pregunta.js
+++ React/presupuestos/src/components/Pregunta.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pregunta = () => {
+  //definir la cantidad
+
+  const [cantidad, setCantidad] = useState(0);
+
+  // state de error
+
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // validacion
+
+    if (cantidad < 1 || isNaN(cantidad)) {
+      setError(true);
+      return;
+    }
+    // validacion exitosa
+    setError(false);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={(e) => setCantidad(parseInt(e.target.value))}
         />
         <input
           type="submit"
