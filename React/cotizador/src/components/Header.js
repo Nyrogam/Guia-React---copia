180fce978bc47548e56bea3defa48d2c8c2f32d2
Ignacio Pino <pino.v.pablo@gmail.com>
2021-08-19 18:32:39 -0300

    creacion de los componentes y estilos de los mismos

--- React/cotizador/src/components/Header.js
+++ React/cotizador/src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const ContenedorHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const TextoHeader = styled.h1`
+  text-align: center;
+  font-size: 2rem;
+  font-family: "Slabo 27px", serif;
+  margin: 0;
+`;
 
 const Header = ({ titulo }) => {
   return (
-    <header>
-      <h1>{titulo}</h1>
-    </header>
+    <ContenedorHeader>
+      <TextoHeader>{titulo}</TextoHeader>
+    </ContenedorHeader>
   );
 };
 
