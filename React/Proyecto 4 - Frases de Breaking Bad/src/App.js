a6da6a55178194ac56fab2b1538dc4268c8b8b2f
Ignacio Pino <pino.v.pablo@gmail.com>
2021-08-24 18:46:18 -0300

    se crea llamado a la api y componente frase conteniendo la misma, se agrega css a frase

--- React/Proyecto 4 - Frases de Breaking Bad/src/App.js
+++ React/Proyecto 4 - Frases de Breaking Bad/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+import Frase from "./components/Frase";
 
 const Contenedor = styled.div`
   display: flex;
@@ -30,13 +31,20 @@ const Boton = styled.button`
   }
 `;
 
-const consultarAPI = () => {
-  console.log("prueba");
-};
-
 function App() {
+  const [frase, setFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    setFrase(frase[0]);
+  };
+
   return (
     <Contenedor>
+      <Frase frase={frase} />
       <Boton onClick={consultarAPI}> Obtener frase</Boton>
     </Contenedor>
   );
---
+++ React/Proyecto 4 - Frases de Breaking Bad/src/components/Frase.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const ContenedorFrase = styled.div`
+  padding: 3rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  max-width: 800px;
+`;
+
+const Frase = ({ frase }) => {
+  return (
+    <ContenedorFrase>
+      <h1>{frase.quote}</h1>
+      <p> - {frase.author}</p>
+    </ContenedorFrase>
+  );
+};
+
+export default Frase;
