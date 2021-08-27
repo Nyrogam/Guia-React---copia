c46c1c80f8d9430d6058a8a599e4ba4b4e7d98d7
Ignacio Pino <pino.v.pablo@gmail.com>
2021-08-27 18:52:04 -0300

    creacion de formulario y llamado a la api

--- React/p5-app_clima/src/App.js
+++ React/p5-app_clima/src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
+  // state principal
+
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  // states para consultar a la api
+
+  const [consulta, setConsulta] = useState(false);
+
+  const [clima, setClima] = useState([]);
+
+  //destructuring
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultaAPI = async () => {
+      if (consulta) {
+        const appId = "1cf7a9f9a4154aec1194adf02176dbb0";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        console.log(url);
+        const api = await fetch(url);
+        const frase = await api.json();
+        console.log(frase);
+      }
+    };
+    consultaAPI();
+  }, [consulta, ciudad, pais]);
+
   return (
     <>
       <Header titulo="Clima React App" />
@@ -9,7 +41,13 @@ function App() {
       <div className="contenedor-form">
         <div className="container">
           <div className="row">
-            <div className="col m6 s12">1</div>
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsulta={setConsulta}
+              />
+            </div>
             <div className="col m6 s12">2</div>
           </div>
         </div>
